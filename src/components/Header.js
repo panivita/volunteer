fefbe1215df4fdd93ea48173f7de3beb39348ce5
VictoriaKush <victoria.v.kush@gmail.com>
2020-11-07 00:09:12 +0100

    added header

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+import Logo from "../images/logo.png";
+import "./custom.css";
+
+export const Header = () => {
+  return (
+    <Navbar  expand="lg" >
+      
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto custom-nav" bsStyle="tabs">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#about">Events</Nav.Link>
+        </Nav>
+        <Navbar.Brand href="#home">
+        <img
+          src={Logo}
+          width="100"
+          height="70"
+          className="d-inline-block align-top"
+          alt="volonteer organization logo"
+        />
+      </Navbar.Brand>
+        <Nav className="custom-nav">
+          <Nav.Link href="#contact">Contact</Nav.Link>
+          <Nav.Link href="#volunteer">Volunteer</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
